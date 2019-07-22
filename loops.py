bf424b3c9877c102518bc81c6e93482834859f65
QiqeMtz <lejm.18@gmail.com>
2019-07-22 00:35:48 -0500

    Updated loops samples

--- loops.py
+++ loops.py
@@ -25,3 +25,38 @@ print("\n5th loop")
 print("I dont care about the i value in the loop, i just need to repeat x times an action")
 for _ in range(10):
     print("Hello")
+
+for i in range(0,15,3):
+    print(i)
+
+for i in range(1000,10,-10):
+    print(i)
+
+names = ["John", "Luis", "Sam"]
+
+for name in names:
+    print(name)
+
+name = names[0]
+
+for letter in name:
+    print(letter)
+
+# Nested loop
+nums = [1,2,3]
+letters = ['a', 'b', 'c']
+
+for num in nums:
+    for letter in letters:
+        print(str(num) + letter)
+
+# Iterate over list of lists
+
+lists = [ ['1','2','3'], ['a','b','c'], ['+','-','='] ]
+
+for list in lists:
+    print(list)
+
+for list in lists:
+    for item in list:
+        print(item)

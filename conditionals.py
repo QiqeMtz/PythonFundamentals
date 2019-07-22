d24545c4ab450e520aa9fad31aedf8a0523159c5
QiqeMtz <lejm.18@gmail.com>
2019-07-22 00:23:11 -0500

    Conditionals

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+grade = 65
+
+if grade > 65:
+    print("Passed!")
+elif grade == 65:
+    print("At least")
+else:
+    print("Not enough")
+
+balance = -5
+
+if balance < 0:
+    print("Account with lower funds")

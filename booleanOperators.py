5fa256a487a70e01e9401425b6ec81d72227cd96
QiqeMtz <lejm.18@gmail.com>
2019-07-22 00:17:33 -0500

    Boolean operations

---
+++ booleanOperators.py
@@ -0,0 +1,17 @@
+x = 5
+y = 8
+
+print("x==y", x==y)
+
+Sammy = "Sammy"
+sammy = "sammy"
+Sammy2 = "Sammy"
+
+print("Sammy==sammy", Sammy==sammy)
+print("Sammy==Sammy2", Sammy==Sammy2)
+
+print((9>7) and (2<4))
+
+print(not(3 <= 1))
+
+print((7.5>7.6) or (-0.5>-1))
